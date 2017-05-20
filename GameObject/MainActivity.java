1b92542028f4d126f010115285e3e05bef27d950
stephensheng <szh13584003824@gmail.com>
2017-05-20 19:37:18 -0400

    Create MainActivity.java
    this is the mainActivity.

---
+++ GameObject/MainActivity.java
@@ -0,0 +1,60 @@
+package com.stephensheng.game2048;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    public MainActivity() {
+        mainActivity = this;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvScore = (TextView) findViewById(R.id.tvScore);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void clearScore(){
+        score = 0;
+        showScore();
+    }
+
+    public void showScore(){
+        tvScore.setText(score+"");
+    }
+
+    public void addScore(int s){
+        score+=s;
+        showScore();
+    }
+
+    private int score = 0;
+    private TextView tvScore;
+
+    private static MainActivity mainActivity = null;
+
+    public static MainActivity getMainActivity() {
+        return mainActivity;
+    }
+
+}
